3b84360c930d70bc6dc8fbfec4d645a1ff17f0ed
Elena Pronichkina <prolena39@mail.ru>
2023-08-26 23:10:59 +0300

    create database.ini.example.py

--- config.example.py
+++
@@ -1,6 +0,0 @@
-[postgresql]
-HOST: str  = "host_name" #имя хоста, на котором находится база данных (обычно localhost)
-PORT: int #номер порта, на котором работает PostgreSQL (обычно 5432)
-DATABASE: str = "database_name" #имя базы данных, к которой вы хотите подключиться
-USER: str = "user_name" #имя пользователя, с которым вы хотите подключиться к базе данных (обычно postgres)
-PASSWORD = "your_password" #пароль пользователя
---
+++ database.ini.example.py
@@ -0,0 +1,9 @@
+[postgresql]
+host: str  = "host_name" #имя хоста, на котором находится база данных (обычно localhost)
+user: str = "user_name" #имя пользователя, с которым вы хотите подключиться к базе данных (обычно postgres)
+password = "your_password" #пароль пользователя
+port: int #номер порта, на котором работает PostgreSQL (обычно 5432)
+
+
+
+
