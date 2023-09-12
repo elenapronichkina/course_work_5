1d340c9f46745cc30a4ae195b97902f4ac710243
Elena Pronichkina <prolena39@mail.ru>
2023-09-12 16:09:10 +0300

    delete files

--- env.example.py
+++
@@ -1,9 +0,0 @@
-[postgresql]
-HOST: str  = "host_name" #имя хоста, на котором находится база данных (обычно localhost)
-USER: str = "user_name" #имя пользователя, с которым вы хотите подключиться к базе данных (обычно postgres)
-PASSWORD = "your_password" #пароль пользователя
-PORT: int #номер порта, на котором работает PostgreSQL (обычно 5432)
-
-
-
-
